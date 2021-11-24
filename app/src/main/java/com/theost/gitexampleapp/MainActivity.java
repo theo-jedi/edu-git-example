75951b1e5c4b1af3516c910f39ea9ae948f84ea2
theo-jedi <feds.msc@gmail.com>
2021-11-24 18:37:36 +0300

    Initial commit

---
+++ app/src/main/java/com/theost/gitexampleapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.theost.gitexampleapp;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(view -> showToast());
+    }
+
+    private void showToast() {
+        Toast.makeText(this, R.string.text, Toast.LENGTH_SHORT).show();
+    }
+
+}
